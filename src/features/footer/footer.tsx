7f43869447f3d2adb8edb99129cc0634127b1ffb
Konstantin Zhitkov <kzhitkov@adsterra.com>
2024-04-11 02:22:12 +0300

    feat(footer): add footer

---
+++ src/features/footer/footer.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import styles from './footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer className={styles['footer']}>
+      <span className={styles['social']}>
+        <Link className={styles['social-link']} href="https://www.behance.net/tkchv">
+          беханс
+        </Link>
+      </span>
+      <span className={styles['social']}>
+        <Link className={styles['social-link']} href="mailto:tkchv@bk.ru">
+          почта
+        </Link>
+      </span>
+    </footer>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import type { AppProps } from "next/app";
 import { Header } from "@/features/header";
+import { Footer } from "@/features/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
       <Header siteTitle={pageProps.data?.global?.title} pageTitle={pageProps.data?.project?.title} />
+      <Footer />
       <Component {...pageProps} />
     </div>
   );
