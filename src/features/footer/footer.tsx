e08d7be6763b025ab68ffa209d8c90db5bad2329
Konstantin Zhitkov <kzhitkov@adsterra.com>
2025-06-22 21:04:41 +0300

    fix(footer): add target="_blank" to social links for external navigation

--- src/features/footer/footer.tsx
+++ src/features/footer/footer.tsx
@@ -5,17 +5,22 @@ export const Footer = () => {
   return (
     <footer className={styles['footer']}>
       <span className={styles['social']}>
-        <Link className={styles['social-link']} href="https://www.behance.net/tkchv">
+        <Link className={styles['social-link']} href="https://vikatkacheva.notion.site/74e073d752d84cda979922c8aa03a9d4" target="_blank">
+          cv
+        </Link>
+      </span>
+      <span className={styles['social']}>
+        <Link className={styles['social-link']} href="https://www.behance.net/tkchv" target="_blank">
           беханс
         </Link>
       </span>
       <span className={styles['social']}>
-        <Link className={styles['social-link']} href="https://t.me/tkchv">
+        <Link className={styles['social-link']} href="https://t.me/tkchv" target="_blank">
           телеграм
         </Link>
       </span>
       <span className={styles['social']}>
-        <Link className={styles['social-link']} href="mailto:tkchv@bk.ru">
+        <Link className={styles['social-link']} href="mailto:tkchv@bk.ru" target="_blank">
           почта
         </Link>
       </span>
