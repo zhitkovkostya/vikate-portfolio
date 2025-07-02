8cbd4bc8c302d8711ed978d8f68e283209317aab
Konstantin Zhitkov <kzhitkov@adsterra.com>
2025-07-02 14:02:16 +0300

    fix(footer): remove outdated CV link from footer

--- src/features/footer/footer.tsx
+++ src/features/footer/footer.tsx
@@ -5,11 +5,6 @@ export const Footer = () => {
   return (
     <footer className={styles['footer']}>
       <span className={styles['social']}>
-        <Link className={styles['social-link']} href="https://vikatkacheva.notion.site/74e073d752d84cda979922c8aa03a9d4" target="_blank">
-          cv
-        </Link>
-      </span>
-      <span className={styles['social']}>
         <Link className={styles['social-link']} href="https://www.behance.net/tkchv" target="_blank">
           беханс
         </Link>
